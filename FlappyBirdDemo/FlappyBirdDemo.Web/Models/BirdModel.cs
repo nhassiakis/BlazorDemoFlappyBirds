7318ce27fbef8bd628efc3f730adb164b4f3a980
Nicolas Hassiakis <40218955+nhassiakis@users.noreply.github.com>
2021-08-20 15:17:19 +0200

    Added jump to bird

--- FlappyBirdDemo/FlappyBirdDemo.Web/Models/BirdModel.cs
+++ FlappyBirdDemo/FlappyBirdDemo.Web/Models/BirdModel.cs
@@ -8,10 +8,15 @@ namespace FlappyBirdDemo.Web.Models
     public class BirdModel
     {
         public int DistanceFromGround { get; set; } = 100;
+        
 
         public void Fall(int gravity)
         {
             DistanceFromGround -= gravity;
+        }
+        public void Jump()
+        {
+            DistanceFromGround += 10;
         }
     }
 }
